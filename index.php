d88c05be0225e2496fe725d2861d0dc38d028ebc
banel1ng <ohot2015@gmail.com>
2017-02-23 12:59:14 +0300

    server gotov

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo 'hellow world';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Redstart</title>
+	<link rel="stylesheet" href="dist/styles/main.css">
+</head>
+<body>
+
+</body>
+</html>
